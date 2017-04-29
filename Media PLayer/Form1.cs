b5b6910d2a1efe2210d9d60ec5ceeb0a2c923d75
Coka <stefan.petrushevski.coka@gmail.com>
2017-04-29 18:56:08 +0200

    Add label

--- Media PLayer/Form1.cs
+++ Media PLayer/Form1.cs
@@ -16,5 +16,10 @@ namespace Media_PLayer
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
