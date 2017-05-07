54f0a1740cd5ccd86ca3641aa988d90ecb152bf6
Dragan Apostolski <drapostolski@gmail.com>
2017-05-08 00:37:04 +0200

    Add media abstraction

---
+++ Media PLayer/Media.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using WMPLib;
+
+namespace Media_PLayer
+{
+    public abstract class Media
+    {
+        public string PathToFile { get; set; }
+
+        protected Media(string pathToFile)
+        {
+            PathToFile = pathToFile;
+        }
+
+    }
+}
---
+++ Media PLayer/MusicFile.cs
@@ -0,0 +1,24 @@
+﻿using Media_PLayer.Structures;
+
+namespace Media_PLayer
+{
+    public class MusicFile : Media
+    {
+        private Song Song { get; set; }
+
+        public string AlbumCover { get; set; }
+
+        public string Url => PathToFile;
+
+        public MusicFile(string pathToFile, uint trackNumber) : base(pathToFile)
+        {
+            TagLib.File file = TagLib.File.Create(pathToFile);
+            Song = new Song(pathToFile, file.Tag.Title, file.Tag.Genres, trackNumber, file.Properties.Duration);
+        }
+
+        public override string ToString()
+        {
+            return Song.ToString();
+        }
+    }
+}
