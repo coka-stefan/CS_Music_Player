df4e284a57426fb46ce6e5274e9f14a5988452de
Dragan Apostolski <drapostolski@gmail.com>
2017-05-08 00:37:34 +0200

    Reorganize things

---
+++ Media PLayer/MainForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Media_PLayer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.pbAlbumCover = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openRecentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playlistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playlistToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.playlistToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbOpenedFiles = new System.Windows.Forms.ListBox();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAlbumCover)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(53, 436);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(119, 41);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // pbAlbumCover
+            // 
+            this.pbAlbumCover.Location = new System.Drawing.Point(24, 40);
+            this.pbAlbumCover.Margin = new System.Windows.Forms.Padding(2);
+            this.pbAlbumCover.Name = "pbAlbumCover";
+            this.pbAlbumCover.Size = new System.Drawing.Size(463, 383);
+            this.pbAlbumCover.TabIndex = 4;
+            this.pbAlbumCover.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.playlistToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(821, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.openFilesToolStripMenuItem,
+            this.openFolderToolStripMenuItem,
+            this.openRecentToolStripMenuItem,
+            this.playlistToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.fileToolStripMenuItem.Text = "Music";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.OpenMusicFileToolStripMenuItem_Click);
+            // 
+            // openFilesToolStripMenuItem
+            // 
+            this.openFilesToolStripMenuItem.Name = "openFilesToolStripMenuItem";
+            this.openFilesToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFilesToolStripMenuItem.Text = "Open Files";
+            this.openFilesToolStripMenuItem.Click += new System.EventHandler(this.openFilesToolStripMenuItem_Click);
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            // 
+            // openRecentToolStripMenuItem
+            // 
+            this.openRecentToolStripMenuItem.Name = "openRecentToolStripMenuItem";
+            this.openRecentToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openRecentToolStripMenuItem.Text = "Recent";
+            // 
+            // playlistToolStripMenuItem
+            // 
+            this.playlistToolStripMenuItem.Name = "playlistToolStripMenuItem";
+            this.playlistToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.playlistToolStripMenuItem.Text = "Playlist";
+            // 
+            // playlistToolStripMenuItem1
+            // 
+            this.playlistToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playlistToolStripMenuItem2});
+            this.playlistToolStripMenuItem1.Name = "playlistToolStripMenuItem1";
+            this.playlistToolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.playlistToolStripMenuItem1.Text = "View";
+            // 
+            // playlistToolStripMenuItem2
+            // 
+            this.playlistToolStripMenuItem2.Name = "playlistToolStripMenuItem2";
+            this.playlistToolStripMenuItem2.Size = new System.Drawing.Size(111, 22);
+            this.playlistToolStripMenuItem2.Text = "Playlist";
+            // 
+            // lbOpenedFiles
+            // 
+            this.lbOpenedFiles.FormattingEnabled = true;
+            this.lbOpenedFiles.Location = new System.Drawing.Point(521, 40);
+            this.lbOpenedFiles.Name = "lbOpenedFiles";
+            this.lbOpenedFiles.Size = new System.Drawing.Size(288, 446);
+            this.lbOpenedFiles.TabIndex = 6;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Image = ((System.Drawing.Image)(resources.GetObject("btnPause.Image")));
+            this.btnPause.Location = new System.Drawing.Point(196, 436);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(124, 41);
+            this.btnPause.TabIndex = 7;
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(348, 436);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(124, 41);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 502);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.lbOpenedFiles);
+            this.Controls.Add(this.pbAlbumCover);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.Text = "Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.pbAlbumCover)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.PictureBox pbAlbumCover;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openRecentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playlistToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playlistToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem playlistToolStripMenuItem2;
+        private System.Windows.Forms.ListBox lbOpenedFiles;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Media PLayer/MainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Media_PLayer.Structures;
+
+namespace Media_PLayer
+{
+    public partial class MainForm : Form
+    {
+        private Media CurrentMedia { get; set; }
+        private Player Player { get; set; } 
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Player = new Player();
+       }
+
+        private void OpenMusicFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+               OpenMusicFile();
+        }
+
+        private void openFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+              
+        }
+
+        private void OpenMusicFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog { Filter = @"Music file (*.mp3)|*.mp3", Title = @"Open Music" };
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            lbOpenedFiles.Items.Clear();
+            pbAlbumCover.ImageLocation = null;
+
+            var directoryPath = Path.GetDirectoryName(ofd.FileName);
+            string albumCover = null;
+            foreach (var file in Directory.GetFiles(directoryPath))
+            {
+                if (file.EndsWith(".jpg") || file.EndsWith(".png") || file.EndsWith(".bmp"))
+                    albumCover = file;
+            }
+
+            MusicFile newMedia = new MusicFile(ofd.FileName, (uint) lbOpenedFiles.Items.Count + 1);
+            newMedia.AlbumCover = albumCover ?? "";
+
+            CurrentMedia = newMedia;
+            DisplayAlbumCover(albumCover);          
+            Player.PlayMusicFile(CurrentMedia);
+            lbOpenedFiles.Items.Add(newMedia);
+        }
+
+
+        private void DisplayAlbumCover(string pathToAlbumCover)
+        {
+            pbAlbumCover.ImageLocation = pathToAlbumCover;
+            pbAlbumCover.ForeColor = ForeColor;
+            pbAlbumCover.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (lbOpenedFiles.SelectedIndex != -1)
+            {
+                Media selectedItem = lbOpenedFiles.SelectedItem as Media;
+                Player.PlayMusicFile(selectedItem);
+                CurrentMedia = selectedItem;
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            Player.Pause();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Player.Pause();
+        }
+    }
+}
