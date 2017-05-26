db1c25d0c272d8f3d0186d4bac79c871b2d7caf8
Coka <stefan.petrushevski.coka@gmail.com>
2017-05-26 18:14:52 +0200

    Build tree beta added

--- Media PLayer/Playlist.cs
+++ Media PLayer/Playlist.cs
@@ -24,7 +24,8 @@ namespace Media_PLayer
             TagLib.File file = TagLib.File.Create(fileName);
             var artist = file.Tag.FirstPerformer;
             var album = file.Tag.Album;
-            Song song = new Song(fileName, file.Tag.Title, artist, album, file.Tag.Genres, (uint) (lb.Items.Count + 1) , file.Properties.Duration);
+            Song song = new Song(fileName, file.Tag.Title, artist, album, file.Tag.Genres, (uint) (lb.Items.Count + 1),
+                file.Properties.Duration);
             AddSongToArtist(artist, album, song);
             lb.Items.Add(song);
         }
@@ -40,15 +41,56 @@ namespace Media_PLayer
         {
             foreach (Artist artist in Artists.Values)
             {
-               artist.Songs.ForEach(song => listBox.Items.Add(song));
+                artist.Songs.ForEach(song => listBox.Items.Add(song));
             }
         }
 
         public void ShowArtistsOnTreeView(TreeView treeView)
         {
             // show artists as root nodes, albums as child nodes to artists nodes, songs as leafs to every album node
-        }
-        
 
+            foreach (Artist artist in Artists.Values)
+            {
+                TreeNode artistNode = new TreeNode(artist.Name);
+                artistNode.Name = artist.Name;
+                artistNode.ImageKey = artist.Name;
+                Boolean artistExists = treeView.Nodes.ContainsKey(artistNode.ImageKey);
+
+                if (artistExists)
+                {
+                    TreeNode artistNodeNew = treeView.Nodes.Find(artist.Name, true)[0];
+
+                    foreach (Album album in artist.Albums.Values)
+                    {
+                        TreeNode albumNodeTest = new TreeNode(album.Name);
+                        albumNodeTest.Name = album.Name;
+                        albumNodeTest.ImageKey = album.Name;
+                        Boolean albumExists = artistNodeNew.Nodes.ContainsKey(albumNodeTest.ImageKey);
+
+                        if (albumExists)
+                        {
+                            TreeNode albumNode = treeView.Nodes.Find(album.Name, true)[0];
+                            foreach (Song song in album.Songs.Values)
+                            {
+                                TreeNode songNode = new TreeNode(song.Title);
+                                songNode.Name = song.Title;
+                                songNode.ImageKey = song.Url;
+                                albumNode.Nodes.Add(songNode);
+                                //                                    playlistTree.Nodes.Add(albumNode.ToString(), song.Title);
+                            }
+                        }
+                        else
+                        {
+                            artistNodeNew.Nodes.Add(albumNodeTest);
+                            //                                playlistTree.Nodes.Add(artistNode.ToString(), album.Name);
+                        }
+                    }
+                }
+                else
+                {
+                    treeView.Nodes.Add(artistNode);
+                }
+            }
+        }
     }
 }
